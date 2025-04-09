ac438c38683b832f7b4e65bbcce5f56ed2b977d9
SK Hamim Ishthiaque Pranto <82573096+Dev-Pranto@users.noreply.github.com>
2025-04-09 14:29:15 +0600

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "@/pages/Home";
+import ProjectsPage from "@/pages/ProjectsPage";
+import ResumePage from "@/pages/ResumePage";
+import PublicationsPage from "@/pages/PublicationsPage";
+import NotFound from "@/pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/resume" element={<ResumePage />} />
+          <Route path="/publications" element={<PublicationsPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
